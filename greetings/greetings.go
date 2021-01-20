fdd725dc0932dc29559fbca31aa35d8ea2271ede
malarc <email@email.com>
2021-01-20 11:48:38 -0500

    Return and handle an error

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -5,11 +5,9 @@ import(
 	"errors"
 	"fmt"
 
-
 )
 
 // Hello returns a greeting for the named person.
-
 func Hello(name string) (string,error) {
 	// If no name was given, return an error with a message.
 
@@ -17,12 +15,10 @@ func Hello(name string) (string,error) {
 		return "", errors.New("empty name")
 	}
 
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-
 	// If a name was recieved, return a value that embeds the name
 	// in a greeting message.
 
-	message = fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message, nil
 
 }
