bb41af23c6bba9e48b88d8d0c337b6229eca6630
malarc <email@email.com>
2021-01-20 12:45:11 -0500

    break greetings.Hello fn to view a failing test

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -14,9 +14,11 @@ func Hello(name string) (string, error) {
         return name, errors.New("empty name")
     }
     // Create a message using a random format.
-    message := fmt.Sprintf(randomFormat(), name)
+    // message := fmt.Sprintf(randomFormat(), name)
+    message := fmt.Sprint(randomFormat())
     return message, nil
 }
+
 
 // Hellos returns a map that associates each of the named people
 // with a greeting message.
